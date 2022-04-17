848ca961f10710a17b168d244e6ce3671209b5e2
Domagoj Starcevic <domagoj.starcevic1@gmail.com>
2022-04-17 20:02:55 +0200

    Added Event Aggregation
    Added Event Aggregation in WPF. Created empty LogOnEvent. Added handle for LogOnEvent.

--- TRMDesktopUI/ViewModels/ShellViewModel.cs
+++ TRMDesktopUI/ViewModels/ShellViewModel.cs
@@ -2,18 +2,35 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using Caliburn.Micro;
+using TRMDesktopUI.EventModels;
 
 namespace TRMDesktopUI.ViewModels
 {
-    public class ShellViewModel : Conductor<object>
+    public class ShellViewModel : Conductor<object>, IHandle<LogOnEvent>
     {
-        private LoginViewModel _loginVM;
-        public ShellViewModel(LoginViewModel loginVM)
+        private IEventAggregator _events;
+        private SalesViewModel _salesVM;
+        private SimpleContainer _container;
+        public ShellViewModel(IEventAggregator events, SalesViewModel salesVM,
+            SimpleContainer container)
         {
-            _loginVM = loginVM;
-            ActivateItemAsync(_loginVM);
+            _events = events;
+            _salesVM = salesVM;
+            _container = container;
+            // TODO - Check if code is OK, in video it was this:
+            //_events.Subscribe(this);
+            //replaced with following:
+            _events.SubscribeOnPublishedThread(this);
+            // TODO - Check if async is OK
+            ActivateItemAsync(_container.GetInstance<LoginViewModel>());
+        }
+        // TODO - check if this really works, because we put async etc
+        public Task HandleAsync(LogOnEvent message, CancellationToken cancellationToken)
+        {
+            return ActivateItemAsync(_salesVM);
         }
     }
 }
